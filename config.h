6f0f636bee205763023e595879ef6f27a647c8a4
Nick Luker <76579997+nck3l@users.noreply.github.com>
2025-12-11 01:44:43 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details.
- * You will need a Nerd Font installed to see the icons */
+ * You will need a Nerd Font installed to see the icons /
 
-/* interval between updates (in ms) */
+ * interval between updates (in ms) */
 const unsigned int interval = 1000;
 
 /* text to show if no value can be retrieved */
@@ -9,37 +9,52 @@ static const char unknown_str[] = "n/a";
 
 /* maximum output string length */
 #define MAXLEN 2048
-
 /*
- * function            description                     argument (example)
- *
- * battery_perc        battery percentage              battery name (BAT0)
- * battery_remaining   battery remaining HH:MM         battery name (BAT0)
- * battery_state       battery charging state          battery name (BAT0)
- * cpu_freq            cpu frequency in MHz            NULL
- * cpu_perc            cpu usage in percent            NULL
- * datetime            date and time                   format string (%F %T)
- * disk_free           free disk space in GB           mountpoint path (/)
- * disk_perc           disk usage in percent           mountpoint path (/)
- * disk_total          total disk space in GB          mountpoint path (/)
- * disk_used           used disk space in GB           mountpoint path (/)
- * entropy             available entropy               NULL
- * ipv4                IPv4 address                    interface name (eth0)
- * ipv6                IPv6 address                    interface name (eth0)
- * keyboard_indicators caps/num lock indicators        format string (c?n?) see keyboard_indicators.c
- * load_avg            load average                    NULL
- * netspeed_rx         receive network speed           interface name (wlan0)
- * netspeed_tx         transfer network speed          interface name (wlan0)
- * ram_free            free memory in GB               NULL
- * ram_perc            memory usage in percent         NULL
- * ram_total           total memory size in GB         NULL
- * ram_used            used memory in GB               NULL
- * run_command         custom shell command            command (echo foo)
- * temp                temperature in degree celsius   sensor file (/sys/class/thermal/...)
- * uptime              system uptime                   NULL
- * wifi_essid          WiFi ESSID                      interface name (wlan0)
- * wifi_perc           WiFi signal in percent          interface name (wlan0)
- */
+* function            description                     argument (example)
+*
+* battery_perc        battery percentage              battery name (BAT0)
+* battery_remaining   battery remaining HH:MM         battery name (BAT0)
+* battery_state       battery charging state          battery name (BAT0)
+* cat                 read arbitrary file             path
+* cpu_freq            cpu frequency in MHz            NULL
+* cpu_perc            cpu usage in percent            NULL
+* datetime            date and time                   format string (%F %T)
+* disk_free           free disk space in GB           mountpoint path (/)
+* disk_perc           disk usage in percent           mountpoint path (/)
+* disk_total          total disk space in GB          mountpoint path (/)
+* disk_used           used disk space in GB           mountpoint path (/)
+* entropy             available entropy               NULL
+* gid                 GID of current user             NULL
+* hostname            hostname                        NULL
+* ipv4                IPv4 address                    interface name (eth0)
+* ipv6                IPv6 address                    interface name (eth0)
+* kernel_release      `uname -r`                      NULL
+* keyboard_indicators caps/num lock indicators        format string (c?n?)
+* keymap              layout (variant) of current     NULL
+*                     keymap
+* load_avg            load average                    NULL
+* netspeed_rx         receive network speed           interface name (wlan0)
+* netspeed_tx         transfer network speed          interface name (wlan0)
+* num_files           number of files in a directory  path
+*                                                     (/home/foo/Inbox/cur)
+* ram_free            free memory in GB               NULL
+* ram_perc            memory usage in percent         NULL
+* ram_total           total memory size in GB         NULL
+* ram_used            used memory in GB               NULL
+* run_command         custom shell command            command (echo foo)
+* swap_free           free swap in GB                 NULL
+* swap_perc           swap usage in percent           NULL
+* swap_total          total swap size in GB           NULL
+* swap_used           used swap in GB                 NULL
+* temp                temperature in degree celsius   sensor file (/sys/class/thermal/...) (tz0, tz1, etc.)
+* uid                 UID of current user             NULL
+* up                  interface is running            interface name (eth0)
+* uptime              system uptime                   NULL
+* username            username of current user        NULL
+* vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
+* wifi_essid          WiFi ESSID                      interface name (wlan0)
+* wifi_perc           WiFi signal in percent          interface name (wlan0)
+*/
 
 static const char vol[] = "muted=`wpctl get-volume @DEFAULT_SINK@ | awk '{print $3;}'`; \
                            volume=`wpctl get-volume @DEFAULT_SINK@ | awk '{print $2;}'`; \
@@ -49,30 +64,21 @@ static const char vol[] = "muted=`wpctl get-volume @DEFAULT_SINK@ | awk '{print 
                            fi";
 
 static const char vpn[] =  "if [ $(cat /sys/class/net/tun0/operstate) = 'unknown' ]; then \
-                                printf \" 󰦝\"; \
-                           else printf \" 󰯆 \"; \
+                                printf \"󰯄\"; \
+                           else printf \"󰯆\"; \
                            fi";
 
 static const struct arg args[] = {
 	/* function format          argument */
-	{ keyboard_indicators,      "^c#00ff00^ %s ",           "C?N?" },
-//    { run_command,  "^c#bf616a^^b#000000^%s",              "echo " },
-	{ cpu_perc,     "^c#bf616a^^b#000000^󰻠 %s%% ",          NULL }, // 󰌢
- 	{ run_command,  "^c#bf616a^^b#000000^%s ",              "echo $(sensors | grep CPU | awk '{print $2}')" },
-//    { run_command,  "^c#d08770^^b#bf616a^%s",              "echo " },
-   	{ ram_used,     "^c#d08770^^b#000000^ %s ",            NULL }, // 
-//    { run_command,  "^c#ebcb8b^^b#d08770^%s",              "echo " },
-	{ wifi_perc,	"^c#ebcb8b^^b#000000^󰤨%s%%",          "wlp2s0"}, // 
-	{ run_command,  "^c#ebcb8b^^b#000000^%2s " ,            vpn },
-//    { run_command,  "^c#a3be8c^^b#ebcb8b^%s",              "echo " },
-	{ run_command,  "^c#a3be8c^^b#000000^ %2s ",           vol },
-//    { run_command,  "^c#81A1C1^^b#a3be8c^%s",              "echo " },
-	{ battery_state,"^c#81A1C1^^b#000000^%s",              "BAT0" },
-	{ battery_perc,	"^c#81A1C1^^b#000000^%s%%",            "BAT0" },
-    { battery_remaining, "^c#81A1C1^^b#000000^%s ",         "BAT0" },
-//	{ datetime, 	"^b#6c5280^^c#000000^%2s",             " %a %e %B" },
-//    { run_command,  "^c#6c5280^^b#81A1C1^%s",              "echo " },
-	{ datetime,     "^c#6c5280^^b#000000^%s",              "󰥔 %T " },
-    { run_command,  "^c#ffffff^^b#1076a9^ %s ",             "echo  6.5" },
-    //{ run_command,  "^b#6c5280^^c#000000^%s",            "echo " },
+	{ cpu_perc,     "^c#dc322f^󰻠 %s%%",          NULL },//BF616A
+//  { cpu_freq,     "^c#BF616A^ %sHz",          NULL },
+// 	{ run_command,  "^c#4AF262^ %s",            "echo $(sensors | grep CPU | awk '{print $2}')" },
+    { ram_used,     "^c#b58900^ 󰘚 %sB",           NULL },//EBCB8B
+	{ run_command,  "^c#2aa198^ %2s" ,            vpn },//A3BE8C
+	{ wifi_perc,	"^c#2aa198^ %s%%",          "wlan0"},
+	{ run_command,  "^c#268bd2^  %2s",           vol },//88C0D0
+	{ battery_state,"^c#d33682^ %s",              "BAT0" },//B48EAD
+    { battery_remaining, "^c#d33682^ %s",         "BAT0" },
+	{ battery_perc,	"^c#d33682^ %s%%",            "BAT0" },
+    { datetime, 	"^c#ffffff^ %2s",              "󰥔 %a %e %b %H:%M " },
 };
